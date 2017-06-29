14e767d63888db56a326df4120135b658dc52d1e
Ams <adriansims62781@gmail.com>
2017-06-29 07:12:52 +0100

    Dialog service first attempt

---
+++ RjisFilter/OneStationDialogHelper.cs
@@ -0,0 +1,42 @@
+﻿using MvvmFoundation.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace RjisFilter
+{
+    class OneStationDialogHelper
+    {
+        public ICommand OpenOneStationDialog { get; private set; }
+        public ICommand OpenOneTocDialog { get; private set; }
+
+        public static OneStationDialogHelper Instance { get; private set; } = null;
+
+        public OneStationDialogHelper GetInstance()
+        {
+            if ()
+        }
+
+        private OneStationDialogHelper()
+        {
+            System.Diagnostics.Debug.WriteLine("Constructed");
+            OpenOneStationDialog = new RelayCommand<object>(OpenOneStation);
+            OpenOneTocDialog = new RelayCommand<object>(OpenOneToc);
+        }
+
+        private void OpenOneStation(object o)
+        {
+            System.Diagnostics.Debug.WriteLine("One");
+        }
+        private void OpenOneToc(object selectedItems)
+        {
+            var collection = (System.Collections.IList)selectedItems;
+            var tocList = collection.Cast<string>().ToList();
+            System.Diagnostics.Debug.WriteLine($"jello");
+        }
+
+    }
+}

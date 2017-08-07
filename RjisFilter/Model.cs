0d176d563c259aaa941d53820e618545e4fdc077
Adrian Sims <asims@parkeon.com>
2017-08-07 13:14:15 +0100

    Small changes

--- RjisFilter/Model.cs
+++ RjisFilter/Model.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace RjisFilter
 {
+
     class Model
     {
         //private Settings settings;
---
+++ RjisFilter/X.cs
@@ -0,0 +1,7 @@
+﻿namespace RjisFilter
+{
+    class X
+    {
+
+    }
+}
